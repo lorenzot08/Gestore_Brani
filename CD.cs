e4c278f3e93296ef3412f17799e582f18a466081
lorenzot08 <lorenzo.torcolacci.stud@ispascalcomandini.it>
2025-10-31 09:37:49 +0100

    R0.1: Prima versione della classe CD

--- CD.cs
+++ CD.cs
@@ -8,5 +8,44 @@ namespace Gestore_Brani_CD
 {
     class CD
     {
+        private string _titolo;
+        private string _autore;
+        private List<Brano> _brani;
+
+        public CD(string titolo, string autore)
+        {
+            _titolo = titolo;
+            _autore = autore;
+            _brani = new List<Brano>();
+        }
+        public string getTitolo()
+        {
+            return _titolo;
+        }
+        public void setTitolo(string titolo)
+        {
+            _titolo = titolo;
+        }
+        public string getAutore()
+        {
+            return _autore;
+        }
+        public void setAutore(string autore)
+        {
+            _autore = autore;
+        }
+        public void AggiungiBrano(Brano br)
+        {
+            _brani.Add(br);
+        }
+        public double Durata()
+        {
+            double totale = 0;
+            foreach (Brano br in _brani)
+            {
+                totale += br.getDurata();
+            }
+            return totale;
+        }
     }
 }
