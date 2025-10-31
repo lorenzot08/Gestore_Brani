a4aa5c3c3eb15336130ea3f5b66b8a7531c70ee3
lorenzot08 <lorenzo.torcolacci.stud@ispascalcomandini.it>
2025-10-31 08:55:36 +0100

    Inizio progetto

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Gestore_Brani_CD
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
