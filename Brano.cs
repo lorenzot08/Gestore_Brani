d6c7a6da173ccef0241f16e94cbcf4de46b02644
Matteo Lucchi <matteo.lucchi@ispascalcomandini.it@github.com>
2025-11-07 08:39:23 +0100

    R0.1:Completata la classe Brano

--- Brano.cs
+++ Brano.cs
@@ -8,5 +8,58 @@ namespace Gestore_Brani_CD
 {
     class Brano
     {
+        string titolo, autore;
+        int durata;
+
+        //Costruttore
+        public Brano(string titolo, string autore, int durata)
+        {
+            this.titolo = titolo;
+            this.autore = autore;
+            this.durata = durata;
+        }
+
+        //returna il titolo
+        public string getTitolo()
+        {
+            return titolo;
+        }
+
+        //returna l'autore
+        public string getAutore()
+        {
+            return autore;
+        }
+
+        //returna la durata
+        public int getDurata()
+        {
+            return durata;
+        }
+
+        //modifica il titolo
+        public void setTitolo(string titolo)
+        {
+            this.titolo = titolo;
+
+        }
+
+        //modifica l'autore
+        public void setAutore(string autore)
+        {
+            this.autore = autore;
+        }
+
+        //returna la stringa con tutte le informazioni del brano
+        public string toString()
+        {
+            return titolo + " " + autore + " " + durata;
+        }
+
+        //chiedi in input una durata e restituisce true se la durata del brano è inferiore alla durata presa in input
+        public bool shortSong(int durata_2)
+        {
+            return (durata_2 > durata);
+        }
     }
 }
